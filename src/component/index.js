9e6b65dc6d957332d7dec2fd606b4ea188b9281b
swensun <1416316137@qq.com>
2017-12-25 14:39:10 +0800

    init

---
+++ src/component/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import './index.less'
+import {Button} from 'antd'
+
+
+const Home = () => {
+    return(
+        <div className='Home'>
+            <Button type="primary">Button</Button>
+            <div>react demo</div>
+            <div>react demo</div>
+        </div>
+    );
+}
+
+export default Home
