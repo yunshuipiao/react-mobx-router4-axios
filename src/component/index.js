4edef5200c326008fd8c4239b6a88513989aaad9
swensun <1416316137@qq.com>
2018-01-10 16:12:50 +0800

    update lifecycle

--- src/component/index.js
+++ src/component/index.js
@@ -3,14 +3,72 @@ import './index.less'
 import {observer} from 'mobx-react'
 import Count from './Count/index'
 import ChangeName from './Change/index'
+import {Button} from 'antd'
 
+class Hello extends React.Component {
+    constructor(props) {
+        super(props)
+        console.log("constructor")
+        this.state = {
+            age: 0
+        }
+    }
 
+    componentWillMount() {
+        console.log("componentWillMount")
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <div className='Hello'>
+                <div>{this.props.name}</div>
+                <div>{this.state.age}</div>
+                <Button type="primary" onClick={() =>
+                    this.setState({
+                        age: this.state.age === 0 ? 111 : 0
+                    })}
+                >Click</Button>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+    }
+
+    componentWillReceiveProps() {
+        console.log("componentWillReceiveProps")
+    }
+
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate")
+        return true
+    }
+
+    componentWillUpdate() {
+        console.log("componentWillUpdate")
+    }
+
+    componentDidUpdate() {
+        console.log("componentDidUpdate")
+    }
+
+    componentWillUnmount() {
+        console.log("componentWillUnmount")
+    }
+
+    componentDidCatch() {
+        console.log("componentDidCatch")
+    }
+}
 
 const Home = observer(() => {
     return (
         <div className='Home'>
-            <Count />
-            <ChangeName />
+            <Count/>
+            <ChangeName/>
+            <Hello name="wen"/>
         </div>
     );
 })
