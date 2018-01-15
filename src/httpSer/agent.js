80da13c4b578ec3a31a59ca2d87bef26144845ce
swensun <1416316137@qq.com>
2018-01-15 15:03:21 +0800

    update catch

--- src/httpSer/agent.js
+++ src/httpSer/agent.js
@@ -35,9 +35,27 @@ const requests = {
                 return res.data
             })
             // also catch in this。but not recommend
-            .catch()
-    // post:
-    // put:
+            .catch( error => {
+                // Error
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+                console.log(error.config);
+            })
+    // post:(body)
+    // put: (body)
     // delete:
 }
 
